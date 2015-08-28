8ae148d95c88657612d5a3926a82925c473496f5
François Caillet <Francois.caillet@calopea.com>
2015-08-28 11:40:28 +0200

    Initiale Commit

---
+++ CoreSpotlight-IOS9-Swift2-Test/CoreSpotlight-IOS9-Swift2-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreSpotlight-IOS9-Swift2-Test
+//
+//  Created by François Caillet on 28/08/2015.
+//  Copyright © 2015 François Caillet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoreSpotlight-IOS9-Swift2-Test/CoreSpotlight-IOS9-Swift2-TestTests/CoreSpotlight_IOS9_Swift2_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreSpotlight_IOS9_Swift2_TestTests.swift
+//  CoreSpotlight-IOS9-Swift2-TestTests
+//
+//  Created by François Caillet on 28/08/2015.
+//  Copyright © 2015 François Caillet. All rights reserved.
+//
+
+import XCTest
+@testable import CoreSpotlight_IOS9_Swift2_Test
+
+class CoreSpotlight_IOS9_Swift2_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
